311b0fff505d55b792a8ce1263a60ece32545c6a
MinexAutomation <minex.automation@gmail.com>
2020-01-21 01:31:51 -0800

    Added IServiceCollection extensions.

--- source/R5T.Kalamaria.Standard/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Kalamaria.Standard
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Kalamaria.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Kalamaria.Default;
+
+
+namespace R5T.Kalamaria.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddOrganizationsDirectoryNameConvention(this IServiceCollection services)
+        {
+            services.AddSingleton<IOrganizationsDirectoryNameConvention, OrganizationsDirectoryNameConvention>();
+
+            return services;
+        }
+    }
+}
